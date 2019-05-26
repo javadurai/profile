a087d0c6783491edad25be26ff59cc9ece9f781c
Jabadurai Selvaraj <jabadurai.s@gmail.com>
2019-05-26 03:40:08 -0500

    Facebook comment added

--- src/components/facebook-comments.js
+++ src/components/facebook-comments.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { FacebookProvider, Comments } from "react-facebook";
+import { FacebookProvider, Comments, Like, ShareButton } from "react-facebook";
 
 export default class FacebookComments extends React.Component {
   render() {
     return (
       <FacebookProvider appId="2336732919871853">
+        <Like
+          href="https://www.facebook.com/Jabadurais-Resume-377891183070986"
+          colorScheme="dark"
+          showFaces
+          share
+        />
         <Comments href="https://www.facebook.com/Jabadurais-Resume-377891183070986" />
       </FacebookProvider>
     );
