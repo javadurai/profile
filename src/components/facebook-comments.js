319275d4f5aeb85287103f4644a441389fb3a916
Jabadurai Selvaraj <jabadurai.s@gmail.com>
2019-05-26 03:27:02 -0500

    Facebook comment added

--- src/components/facebook-comments.js
+++ src/components/facebook-comments.js
@@ -5,7 +5,7 @@ export default class FacebookComments extends React.Component {
   render() {
     return (
       <FacebookProvider appId="2336732919871853">
-        <Comments href="http://www.facebook.com" />
+        <Comments href="https://www.facebook.com/Jabadurais-Resume-377891183070986" />
       </FacebookProvider>
     );
   }
